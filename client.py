a830921978f979ae8fce54ef55d55342b6adb06e
Teba <teba@arkc.org>
2015-07-28 11:10:02 +0800

    Allow multiple proxy connections with one client connection

--- client.py
+++ client.py
@@ -16,18 +16,19 @@ class ClientConnector(Protocol):
     def __init__(self, initiator):
         self.initiator = initiator
         self.split_char = chr(30) * 5
+        self.close_char = chr(4) * 5
         self.pw_gen = lambda l: ''.join([choice(L + D) for i in range(l)])
         self.main_pw = self.initiator.main_pw
         self.pri = self.initiator.initiator.pri
         self.client_pub = self.initiator.client_pub
         self.session_pw = self.pw_gen(16)
         self.cipher = AES.new(self.session_pw, AES.MODE_CFB, self.main_pw)
-        self.buffer = ""
-        self.write_queue = deque()
+        self.buffers = dict()
+        self.write_queues = dict()
         self.proxy_port = self.initiator.initiator.proxy_port
-        self.proxy_connector = ProxyConnector(self)
-        point = TCP4ClientEndpoint(reactor, "127.0.0.1", self.proxy_port)
-        connectProtocol(point, self.proxy_connector)
+        self.proxy_connectors = dict()
+        host, port = "127.0.0.1", self.proxy_port
+        self.proxy_point = TCP4ClientEndpoint(reactor, host, port)
 
     def generate_auth_msg(self):
         """Generate encrypted message.
@@ -41,32 +42,65 @@ class ClientConnector(Protocol):
         pw_enc = self.client_pub.encrypt(self.session_pw, None)[0]
         return hex_sign + pw_enc
 
+    def new_proxy_conn(self, conn_id):
+        try:
+            assert conn_id not in self.buffers
+            self.buffers[conn_id] = ""
+            self.write_queues[conn_id] = deque()
+            self.proxy_connectors[conn_id] = ProxyConnector(self, conn_id)
+            connectProtocol(self.proxy_point, self.proxy_connectors[conn_id])
+        except AssertionError:
+            logging.error("duplicate id")
+
+    def del_proxy_conn(self, conn_id):
+        try:
+            assert self.buffers.pop(conn_id, None)
+            assert self.write_queues.pop(conn_id, None)
+            assert self.proxy_connectors.pop(conn_id, None)
+        except AssertionError:
+            logging.warning("deleting non-existing key")
+
     def connectionMade(self):
         logging.info("connected to " + str(self.transport.getPeer()))
         self.transport.write(self.generate_auth_msg())
         self.initiator.number += 1
 
-    def dataReceived(self, data):
-        self.buffer += data
-        recv = self.buffer.split(self.split_char)
-        self.write_queue.extend(recv[:-1])
-        self.buffer = recv[-1]  # incomplete message
-        while self.write_queue:
-            self.write()
+    def dataReceived(self, recv_data):
+        conn_id, data = recv_data[:2], recv_data[2:]
+        if data == self.close_char:
+            pass    # TODO: implement this behavior
+        else:
+            if conn_id not in self.buffers:
+                self.new_proxy_conn(conn_id)
+            self.buffers[conn_id] += data
+            recv = self.buffer.split(self.split_char)
+            self.write_queues[conn_id].extend(recv[:-1])
+            self.buffers[conn_id] = recv[-1]  # incomplete message
+            while self.write_queues[conn_id]:
+                self.write(conn_id)
 
-    def write(self):
-        write_buffer = self.cipher.decrypt(self.write_queue.popleft())
-        self.proxy_connector.transport.write(write_buffer)
+    def write(self, conn_id):
+        buffer_enc = self.write_queues[conn_id].popleft()
+        write_buffer = self.cipher.decrypt(buffer_enc)
+        self.proxy_connectors[conn_id].transport.write(write_buffer)
 
-    def write_client(self, data):
-        to_write = self.cipher.encrypt(data) + self.split_char
+    def finish(self, conn_id):
+        self.write_client(self.close_char, conn_id)
+        self.del_proxy_conn(conn_id)
+
+    def clean(self):
+        for conn_id in self.buffers.keys():
+            while self.write_queues[conn_id]:
+                self.write(conn_id)
+
+    def write_client(self, data, conn_id):
+        to_write = self.cipher.encrypt(conn_id + data) + self.split_char
         self.transport.write(to_write)
 
     def connectionLost(self, reason):
         logging.info("client connection lost with " + str(reason))
         self.initiator.number -= 1
-        while self.write_queue:
-            self.write()
+        self.clean()
 
 
 class ClientConnectorCreator:
--- proxy.py
+++ proxy.py
@@ -5,12 +5,13 @@ from twisted.internet.protocol import Protocol
 
 class ProxyConnector(Protocol):
 
-    def __init__(self, initiator):
+    def __init__(self, initiator, conn_id):
         self.initiator = initiator
+        self.conn_id = conn_id
         self.split_char = chr(30) * 5
         self.buffer = ''
         self.write_queue = deque()
-        self.segment_size = 4096
+        self.segment_size = 4094    # 4096(total)-2(id)
 
     def connectionMade(self):
         logging.info("connected to " + str(self.transport.getPeer()))
@@ -30,10 +31,10 @@ class ProxyConnector(Protocol):
 
     def write(self):
         write_buffer = self.write_queue.popleft()
-        self.initiator.write_client(write_buffer)
+        self.initiator.write_client(write_buffer, self.conn_id)
 
     def connectionLost(self, reason):
         logging.info("target connection lost with " + str(reason))
         while self.write_queue:
             self.write()
-        self.initiator.transport.loseConnection()
+        self.initiator.finish(self.conn_id)
