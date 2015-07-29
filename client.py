6ea0fc4224aed1053be53cc1a66a612428c3ce62
Teba <teba@arkc.org>
2015-07-29 22:21:18 +0800

    Decrypt before slicing id

--- client.py
+++ client.py
@@ -23,8 +23,8 @@ class ClientConnector(Protocol):
         self.client_pub = self.initiator.client_pub
         self.session_pw = self.pw_gen(16)
         self.cipher = AES.new(self.session_pw, AES.MODE_CFB, self.main_pw)
-        self.buffers = dict()
-        self.write_queues = dict()
+        self.buffer = ""
+        self.write_queues = dict()  # stores decrypted data
         self.proxy_port = self.initiator.initiator.proxy_port
         self.proxy_connectors = dict()
         host, port = "127.0.0.1", self.proxy_port
@@ -45,8 +45,7 @@ class ClientConnector(Protocol):
     def new_proxy_conn(self, conn_id):
         logging.info("adding connection id " + conn_id)
         try:
-            assert conn_id not in self.buffers
-            self.buffers[conn_id] = ""
+            assert conn_id not in self.write_queues
             self.write_queues[conn_id] = deque()
             self.proxy_connectors[conn_id] = ProxyConnector(self, conn_id)
             connectProtocol(self.proxy_point, self.proxy_connectors[conn_id])
@@ -56,7 +55,6 @@ class ClientConnector(Protocol):
     def del_proxy_conn(self, conn_id):
         logging.info("deleting connection id " + conn_id)
         try:
-            assert self.buffers.pop(conn_id, None)
             assert self.write_queues.pop(conn_id, None)
             assert self.proxy_connectors.pop(conn_id, None)
         except AssertionError:
@@ -68,30 +66,35 @@ class ClientConnector(Protocol):
         self.initiator.number += 1
 
     def dataReceived(self, recv_data):
-        conn_id, data = recv_data[:2], recv_data[2:]
-        if data == self.close_char:
-            pass    # TODO: implement this behavior
-        else:
-            if conn_id not in self.buffers:
-                self.new_proxy_conn(conn_id)
-            self.buffers[conn_id] += data
-            recv = self.buffers[conn_id].split(self.split_char)
-            self.write_queues[conn_id].extend(recv[:-1])
-            self.buffers[conn_id] = recv[-1]  # incomplete message
-            while self.write_queues[conn_id]:
-                self.write(conn_id)
+        self.buffer += recv_data
+        recv = self.buffer.split(self.split_char)
+        touched_ids = set()
+        for text_enc in recv[:-1]:
+            text_dec = self.cipher.decrypt(text_enc)
+            conn_id, data = text_dec[:2], text_dec[2:]
+            if data == self.close_char:
+                pass    # TODO: implement this behavior
+            else:
+                touched_ids.add(conn_id)
+                if conn_id not in self.write_queues:
+                    self.new_proxy_conn(conn_id)
+                self.write_queues[conn_id].append(data)
+        self.main_buf = recv[-1]  # incomplete message
+        for conn_id in touched_ids:
+            self.write(conn_id)
 
     def write(self, conn_id):
-        buffer_enc = self.write_queues[conn_id].popleft()
-        write_buffer = self.cipher.decrypt(buffer_enc)
-        self.proxy_connectors[conn_id].transport.write(write_buffer)
+        """Flush the queue of conn_id."""
+        while self.write_queues[conn_id]:
+            data = self.write_queues[conn_id].popleft()
+            self.proxy_connectors[conn_id].transport.write(data)
 
     def finish(self, conn_id):
         self.write_client(self.close_char, conn_id)
         self.del_proxy_conn(conn_id)
 
     def clean(self):
-        for conn_id in self.buffers.keys():
+        for conn_id in self.write_queues.keys():
             while self.write_queues[conn_id]:
                 self.write(conn_id)
 
