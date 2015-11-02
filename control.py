2ae1e611402542230847e011475290603edcd1e7
Noah <noah@arkc.org>
2015-11-02 12:17:09 +0800

    fix a mistake

--- control.py
+++ control.py
@@ -191,7 +191,7 @@ class Control:
         #conn.transport.loseConnection()
         self.client_lost(conn)
         conn.write(self.close_char, "00", 100)
-        reactor.callLater(1.0, conn.ConnectionLost)
+        reactor.callLater(1.0, conn.connectionLost)
 
     def client_lost(self, conn):
         """Triggered by a ClientConnector's connectionLost method.
