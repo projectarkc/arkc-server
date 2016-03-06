9ae7693b77c785a61e31ac9b19d780ee2d4ee6a6
Tsre <tsre@arkc.org>
2016-03-06 15:34:11 +0800

    add database commit

--- mailcheck/main.py
+++ mailcheck/main.py
@@ -4,6 +4,8 @@ import poplib
 import time
 from email import policy
 from email.parser import BytesParser
+import sqlite3
+from common import certloader
 
 MAILADDR = "service@arkc.org"
 PASSWD = "freedom.arkc.org"
@@ -24,13 +26,19 @@ while True:
     M.user(MAILADDR)
     res = M.pass_(PASSWD)
     if b"OK" in res:
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
         numMessages = M.stat()[0]
         number = 1
         while number < numMessages + 1:
             (server_msg, body, octets) = M.retr(number)
-            sha1, pubkey = parse(body)
-            print(sha1, pubkey)
+            pri_sha1, pubkey = parse(body)
+            pub_sha1 = certloader(pubkey).getSHA1()
+            cur.execute(
+                'insert into test certs(?,?,?)', (pub_sha1, pri_sha1, pubkey))
             M.dele(number)
             number += 1
+        con.commit()
+        con.close()
     M.quit()
     time.sleep(30)
