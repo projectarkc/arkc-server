7138297e79403d10327ac1447183fc25fad59669
Noah <noah@arkc.org>
2016-03-14 23:50:09 +0800

    change to an SMTP server

--- mailcheck/main.py
+++ mailcheck/main.py
@@ -1,16 +1,37 @@
 #! /usr/bin/env python3
+# coding:utf-8
 
-import poplib
+import sys
 import argparse
-import time
+import smtpd
+import asyncore
 from email import policy
 from email.parser import BytesParser
 import sqlite3
 import logging
 from common import certloader
 
+
 class CorruptMail(Exception):
     pass
+
+
+class SMTPserver(smtpd.SMTPServer):
+
+    def process_message(self, peer, mailfrom, rcpttos, data):
+        try:
+            print(data)
+            pri_sha1, pubkey = parse(body)
+            pub_sha1 = certloader(pubkey).getSHA1()
+            cur.execute(
+                'INSERT INTO certs VALUES (?,?,?)', (pub_sha1, pri_sha1, pubkey))
+            con.commit()
+            logging.info("Add client with public sha1 " + pub_sha1)
+        except CorruptMail:
+            logging.warning("Deleting a malformed email.")
+        except Exception:
+            logging.warning("Error processing one email.")
+
 
 def parse(body):
     msg = BytesParser(policy=policy.default).parsebytes(b'\n'.join(body))
@@ -28,39 +49,14 @@ def main():
     parser = argparse.ArgumentParser(description=None)
     # parser.add_argument(
     #    "--version", dest="version", action="store_true", help="show version number")
-    parser.add_argument('-c', '--config', dest="config", required=True,
-                        help="specify a configuration files, required to start")
+    parser.add_argument('-db', '--database', dest="db_path", required=True,
+                        help="specify the database file to use")
     args = parser.parse_args()
 
     logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                         format="%(levelname)s: %(asctime)s; %(message)s")
 
-    # Load json configuration file
-    try:
-        data_file = open(args.config)
-        data = json.load(data_file)
-        data_file.close()
-    except Exception as err:
-        logging.error("Format error while loading configuration file.")
-        print(err)  # TODO: improve error processing
-        sys.exit()
-
-    try:
-        dbpath = data["db_path"]
-        user = data["user"]
-        passwd = data["password"]
-        ssl = data["ssl"]
-        popserver = data["mail_server"]
-        if "mail_port" in data:
-            port = data["mail_server"]
-        elif ssl:
-            port = poplib.POP3_SSL_PORT
-        else:
-            port = poplib.POP3_PORT
-    except Exception as err:
-        logging.error("Broken configuration file.")
-        print(err)
-        sys.exit()
+    dbpath = args.db_path
 
     try:
         con = sqlite3.connect(dbpath)
@@ -70,39 +66,18 @@ def main():
         sys.exit()
 
     cur = con.cursor()
-    cur.execute("CREATE TABLE certs IF NOT EXISTS (pubkey_sha1 text, prikey_sha1, text, pubkey_body text)")
+    cur.execute(
+        "CREATE TABLE certs (pubkey_sha1 text, prikey_sha1, text, pubkey_body text)")
     con.commit()
 
-    while True:
-        if ssl:
-            M = poplib.POP3_SSL(popserver, port)
-        else:
-            M = poplib.POP3(popserver, port)
-        M.user(user)
-        res = M.pass_(passwd)
-        if b"OK" in res:
-            numMessages = M.stat()[0]
-            number = 1
-            while number < numMessages + 1:
-                (server_msg, body, octets) = M.retr(number)
-                try:
-                    pri_sha1, pubkey = parse(body)
-                    pub_sha1 = certloader(pubkey).getSHA1()
-                    cur.execute(
-                                'INSERT INTO certs VALUES (?,?,?)', (pub_sha1, pri_sha1, pubkey))
-                    con.commit()
-                    logging.info("Add client with public sha1 " + pub_sha1)
-                except CorruptMail:
-                    logging.warning("Deleting a malformed email.")
-                except Exception:
-                    logging.warning("Error processing one email.")
-                finally:
-                    M.dele(number)
-                    number += 1
-                
-        M.quit()
-        time.sleep(30)
-    con.close()
+    smtp = SMTPserver(('', 25), None)
+
+    try:
+        asyncore.loop(use_poll=True)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        con.close()
 
 if __name__ == "__main__":
     main()
