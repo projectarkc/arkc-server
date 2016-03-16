2f1f0b38d15d89ecfbeb1d6bb97df5bd84716d26
Noah <noah@arkc.org>
2016-03-16 23:38:37 +0800

    add more check

--- mailcheck/main.py
+++ mailcheck/main.py
@@ -30,18 +30,18 @@ class SMTPserver(smtpd.SMTPServer):
             cur.execute(
                 'INSERT INTO certs VALUES (?,?,?)', (pub_sha1, pri_sha1, pubkey))
             con.commit()
-            logging.info("Add client with public sha1 " + pub_sha1)
+            logging.info("Adding client with public sha1 " + pub_sha1)
         except CorruptMail:
-            logging.warning("Deleting a malformed email.")
+            logging.warning("Ignoring a malformed email.")
         except Exception:
-            logging.warning("Error processing one email.")
+            logging.warning("Error processing an email.")
 
 
 def parse(body):
     p = Parser()
     msg = p.parsestr(body)
-    # if "Conference Registration" not in msgobj['Subject'] and msg.is_multipart():
-    #    raise CorruptMail
+    if "Conference Registration" not in msg['Subject'] or not msg.is_multipart():
+        raise CorruptMail
     attachments = []
     body_text = b""
     body_html = b""
@@ -55,6 +55,8 @@ def parse(body):
         #elif part.get_content_type() == "text/html":
         #    body_html += unicode(part.get_payload(decode=True),
         #                         part.get_content_charset(), 'replace').encode('utf8', 'replace')
+    if len(attachments) == 0 or len(sha1) != 40:
+        raise CorruptMail
     return sha1.decode('UTF-8'), attachments[0]['filedata'].decode('UTF-8')
 
 
@@ -110,7 +112,7 @@ def main():
     except sqlite3.OperationalError:
         pass
 
-    smtp = SMTPserver(('', 25), None, cur, con)
+    smtp = SMTPserver(('', 25), None)
 
     try:
         asyncore.loop(use_poll=True)
